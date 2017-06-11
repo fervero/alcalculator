1ec8a08a0e94b4897a1e4e37a657a4880096392e
Maciej Bojko <maciej.bojko@gmail.com>
2017-06-11 23:38:37 +0200

    Working.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@ var calcApp = angular.module("calcApp", []);
 
 calcApp.service('calcService', function() {
   var ethanolDensity = 0.789;
+  var genderCoefficient = { 
+    "male": 5/3, 
+    "female": 10/7 
+  }
+  var soberingTime = 1 / 0.15;
   this.calculateOneDrink = function(drinksNumber, vol, proof) {
     var calc = drinksNumber * vol * proof / 100;
     return Number.isNaN(calc) ? 0 : calc * ethanolDensity;
+  }
+  this.calculateBAC = function(bodyMass, gender, ethanolMass) {
+    return genderCoefficient[gender] * ethanolMass / bodyMass;
+  }
+  this.calculateSoberingTime = function(bac) {
+    return bac * soberingTime;
   }
 });
 
@@ -18,12 +29,25 @@ calcApp.service('idService', function() {
 calcApp.controller("mainController", ['$scope', 'calcService', 'idService', function($scope, $calcService, $idService) {
   $scope.drinkRecord = [];
   $scope.calculateOneDrink = $calcService.calculateOneDrink;
+  $scope.BAC = $calcService.calculateBAC;
+  $scope.calculateTime = $calcService.calculateSoberingTime;
+  $scope.gender = "male";
+  $scope.bodyMass = 75;
   $scope.availableDrinks = [
     {type: "vodka", proof: 45, vol: 40},
-    {type: "beer", proof: 6, vol: 500}
+    {type: "beer", proof: 5.5, vol: 500},
+    {type: "wine", proof: 12, vol: 100},
+    {type: "whisky, gin, tequila", proof: 40, vol: 40},
+    {type: "liqueur", proof: 18, vol: 40},
+    {type: "dwarven spirit", proof: 100, vol: 40}
   ];
   $scope.addDrink = function() {
-    $scope.drinkRecord.push({ name: $idService.getID() });
+    $scope.drinkRecord.push({ 
+      name: $idService.getID(),
+      number: 1,
+      vol: 0,
+      proof: 0
+    });
   }
   $scope.drinksSum = function() {
     return ($scope.drinkRecord)
@@ -42,7 +66,6 @@ calcApp.controller("mainController", ['$scope', 'calcService', 'idService', func
     $scope.destroyRow(arg);
   });
 }]);
-
 
 calcApp.directive("oneDrink", function() {
   return {
