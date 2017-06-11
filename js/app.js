b077148beca1484a11ac2ef4e9b0fa6e80907c17
Maciej Bojko <maciej.bojko@gmail.com>
2017-06-11 13:02:06 +0200

    Working logic for a single type of drink.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,43 @@
 var calcApp = angular.module("calcApp", []);
 
-calcApp.controller("mainController", function($scope) {
+calcApp.service('calcService', function() {
+  var ethanolDensity = 0.789;
+  this.calculateOneDrink = function(drinksNumber, vol, proof) {
+    var calc = drinksNumber * vol * proof / 100;
+    return Number.isNaN(calc) ? 0 : calc * ethanolDensity;
+  }
+});
+
+calcApp.controller("mainController", ['$scope', 'calcService', function($scope, $calcService) {
   $scope.drinkRecord = [];
-  $scope.message = "Hello";
+  $scope.calculateOneDrink = $calcService.calculateOneDrink;
   $scope.availableDrinks = [
-    {name: "vodka", proof: "45", vol: "40"},
-    {name: "beer", proof: "6", vol: "500"}
+    {type: "vodka", proof: "45", vol: "40"},
+    {type: "beer", proof: "6", vol: "500"}
   ];
   $scope.addDrink = function() {
-    console.log("gulp!");
     $scope.drinkRecord.push({ name: Math.random().toString() });
+  }
+  $scope.drinksSum = function() {
+    return ($scope.drinkRecord)
+      .reduce(
+        function(acc, drink) { 
+          return acc + drink.number 
+        }, 0);
+  }
+}]);
+
+
+calcApp.directive("oneDrink", function() {
+  return {
+    templateUrl: 'views/one-drink.html',
+    require: 'ngModel',
+    replace: true,
+    scope: {
+      drinkObject: "=",
+      drinkList: "=",
+      selectedDrink: "=",
+      calculate: "&"
+    }
   }
 });
